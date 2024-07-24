e03182d9b102af45a1d094cb197ef89a3b05d199
yugsakari20 <yugsakariya976@gmail.com>
2024-07-24 09:37:08 +0530

    yug

---
+++ EXAM/main.js
@@ -0,0 +1,10 @@
+function printString(str) {
+ 
+    for (let i = 0; i < str.length; i++) {
+        console.log(str[i]);
+    }
+}
+
+// Example usage
+const inputString = 'Hello, World!';
+printString(inputString);
